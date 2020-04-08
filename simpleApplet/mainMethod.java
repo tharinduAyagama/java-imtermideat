c377cc6a3d1ffebb5dc91486ca4c508b292cd30b
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-04-08 07:14:43 +0530

    applet

---
+++ simpleApplet/mainMethod.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import javax.swing.*;
+
+class mainMethod extends JApplet{
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawString("simple applet" , 25 , 50);
+    }
+}
